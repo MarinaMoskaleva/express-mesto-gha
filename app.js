519b8123da597815b0a191fc28068c0376e23bfe
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2022-04-11 23:25:02 +0300

    start sprint13

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, err => {
+  if(err) throw err;
+}
+);
+
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
