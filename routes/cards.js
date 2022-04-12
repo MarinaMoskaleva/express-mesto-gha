e6b4c83720c74da418075d1d3a34dd383ff896bb
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2022-04-12 22:41:53 +0300

    test9

--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.get('/:cardId', deleteCard);
+router.delete('/:cardId', deleteCard);
 router.post('/', createCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
