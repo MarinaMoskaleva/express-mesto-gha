cd90daaff8635126127dd40c2cef62adca927325
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2022-04-21 11:40:56 +0300

    test20

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { AVATAR_REGEX } = require('../constants');
+
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom((value, helpers) => {
+      if (AVATAR_REGEX.test(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка');
+    }),
+  }),
+}), createCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
 
