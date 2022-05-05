6a5ba25846e9b8c472135822822a35039a325ee8
MarinaMoskaleva <m.a.maximova1@yandex.ru>
2022-05-05 11:54:54 +0300

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const validator = require('validator');
 const helmet = require('helmet');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+//const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { AVATAR_REGEX } = require('./constants');
 const auth = require('./middlewares/auth');
 const cenralErrors = require('./middlewares/central-err');
@@ -34,7 +34,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   if (err) throw err;
 });
 
-app.use(requestLogger);
+//app.use(requestLogger);
 
 // app.use(function(req, res, next) {
 //   const { origin } = req.headers;
@@ -95,7 +95,7 @@ app.use('/', (req, res, next) => {
   next(new NotFoundError('Неправильный путь.'));
 });
 
-app.use(errorLogger);
+//app.use(errorLogger);
 
 app.use(errors());
 
