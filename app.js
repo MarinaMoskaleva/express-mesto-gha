76a6d77995e4f49788f6e8ddead90e5d224b6270
MarinaMoskaleva <m.a.maximova1@yandex.ru>
2022-05-05 12:07:58 +0300

    cors + logs

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ const {
 } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-err');
 
-// const allowedCors = [
-//   'https://mesto.moskalevam.nomoredomains.work',
-//   'http://mesto.moskalevam.nomoredomains.work',
-//   'localhost:3000'
-// ];
+const allowedCors = [
+  'https://mesto.moskalevam.nomoredomains.work',
+  'http://mesto.moskalevam.nomoredomains.work',
+  'localhost:3000'
+];
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -36,24 +36,24 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 
-// app.use(function(req, res, next) {
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   next();
-// });
+app.use(function(req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
-// app.use(function(req, res) {
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-// });
+app.use(function(req, res) {
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -95,7 +95,7 @@ app.use('/', (req, res, next) => {
   next(new NotFoundError('Неправильный путь.'));
 });
 
-//app.use(errorLogger);
+app.use(errorLogger);
 
 app.use(errors());
 
