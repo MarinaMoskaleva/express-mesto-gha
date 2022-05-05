a78f0302df5cf8dd80b06e5508f8e974d5db7504
MarinaMoskaleva <m.a.maximova1@yandex.ru>
2022-05-05 11:35:43 +0300

    cors2

--- app.js
+++ app.js
@@ -38,18 +38,21 @@ app.use(requestLogger);
 
 app.use(function(req, res, next) {
   const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  next();
+});
+
+app.use(function(req, res) {
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  next();
 });
 
 app.post('/signin', celebrate({
